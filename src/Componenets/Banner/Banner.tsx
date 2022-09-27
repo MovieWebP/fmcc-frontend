af1ed187fbc04b3d8bb9f3934386d67ab9249340
최근원 <kidjustinchoi@gmail.com>
2022-09-27 12:59:02 +0900

    changing hole banner

--- src/Componenets/Banner/Banner.tsx
+++ src/Componenets/Banner/Banner.tsx
@@ -1,62 +1,92 @@
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { IMovie } from "../../Api/api";
+import styled from "styled-components";
 import { makeImagePath } from "../../Api/utils";
 import { modalState } from "../../atom";
 import * as S from "./BannerStyle";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-const rowVariants = {
-    hidden: ({ prev }: { prev: boolean }) => ({
-        x: prev ? "100vw" : "-100vw",
-    }),
-    visible: ({ prev }: { prev: boolean }) => ({
-        x: 0,
-    }),
-    exit: ({ prev }: { prev: boolean }) => ({
-        x: prev ? "-100vw" : "100vw",
-    }),
-}
+const Slide = styled.div`
+  position: relative;
+  width: 100vw;
+  height: calc(100vh - 150px);
+  padding: 50px 100px;
+  /* border: 10px solid white; */
+`;
+
+const Background = styled.div<{ imgUrl: string }>`
+  border: 10px solid red;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${props => props.imgUrl});
+  background-position: center;
+  background-size: cover;
+  opacity: 0.6;
+  z-index: -1;
+`;
+
+const Contents = styled.div`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const Subtitle = styled.h3`
+  margin-bottom: 15px;
+  font-size: 18px;
+  font-weight: 300;
+  text-transform: uppercase;
+  color: #fff;
+`;
+
+const MovieTitle = styled.h2`
+  margin-bottom: 20px;
+  font-size: 54px;
+  text-align: right;
+  color: #e50914;
+  font-weight: 900;
+  text-shadow: 2px 2px 0px #fff, 6px 6px 0px rgb(54 54 54 / 50%);
+`;
+
+const ViewDetailBtn = styled(Link)`
+  display: block;
+  padding: 15px 30px;
+  background: #e50914;
+  color: #fff;
+  font-weight: 600;
+  border-radius: 30px;
+  transition: .3s linear;
+
+  &:hover {
+    background: #fff;
+    color: #e50914;
+  }
+`;
 interface IBanner {
     id: string;
     part: string;
     movies: IMovie[];
 }
 
-const offset = 1;
+interface IProps {
+    props?: any;
+}
 
 function Banner({ part, id, movies }: IBanner) {
-    const [index, setIndex] = useState(0);
-    const [sliderMoving, setSliderMoving] = useState(false);
-    const [sliderMovingPrev, setSliderMovingPrev] = useState(false);
-
-    const totlaLength = movies.length - 16;
-    const maxIndex = Math.floor(totlaLength / offset);
-    // slider + 1
-    const moveSlider = () => {
-        if (!sliderMoving && movies) {
-            setSliderMoving(true);
-            setSliderMovingPrev(false);
-            setIndex((prev) => prev === maxIndex ? 0 : prev + 1);
-        }
-    }
-
-    // slider - 1
-    const moveSliderPrev = () => {
-        if (!sliderMoving && movies) {
-            setSliderMoving(true);
-            setSliderMovingPrev(false);
-            setIndex((prev) => prev === maxIndex ? 0 : prev - 1);
-        }
-    }
-
-    // slider done
-    const ExitMoveSlider = () => {
-        setSliderMoving(false);
-        setSliderMovingPrev(false);
-    }
 
     const [isModalActive, setisModalActive] = useRecoilState(modalState);
     const navigate = useNavigate();
@@ -65,49 +95,78 @@ function Banner({ part, id, movies }: IBanner) {
         setisModalActive(true)
     }
 
-    return (
+    function NextArrow({ props }: IProps) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick}
+            >
+                <FontAwesomeIcon icon={faChevronRight} size="3x" className="slick-arrow-icon-right" />
+            </div>
+        );
+    }
 
-        <S.Wrap>
-            <S.AllWrap>
-                <AnimatePresence
-                    custom={{ prev: sliderMovingPrev }}
-                    initial={false}
-                    onExitComplete={ExitMoveSlider}
-                >
+
+    function PrevArrow({ props }: IProps) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                style={{ ...style, display: "block" }}
+                onClick={onClick}
+            >
+                <FontAwesomeIcon icon={faChevronLeft} size="3x" className="slick-arrow-icon-left" />
+            </div>
+        );
+    }
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        autoplay: true,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />
+    }
+
+    return (
+        <S.StyledSlider {...settings}>
+            {/* <S.Wrap>
+                <S.AllWrap>
                     {movies
-                        ?.slice(1)
-                        .slice(offset * index, offset * index + offset).map((movie) => (
+                        .filter((item, index) => (index >= 10))
+                        .map((item, index) => (
                             < S.MainImage
-                                bgPhoto={makeImagePath(movie?.backdrop_path)}
-                                variants={rowVariants}
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                custom={{ prev: sliderMoving }}
-                                transition={{ type: "tween", duration: 0.5 }}
-                                key={index}
+                                bgPhoto={makeImagePath(item?.backdrop_path)}
                             >
-                                {index === 0 ? null : (
-                                    <S.ArrowBox onClick={moveSliderPrev}>
-                                        <MdKeyboardArrowLeft size="60px" />
-                                    </S.ArrowBox>
-                                )}
-                                <S.RightArrow onClick={moveSlider} >
-                                    <MdKeyboardArrowRight size="60px" />
-                                </S.RightArrow>
-                                <S.BannerImage bgPhoto={makeImagePath(movie?.backdrop_path)}>
+                                <S.BannerImage bgPhoto={makeImagePath(item?.backdrop_path)}>
                                     <S.BannerWrap>
-                                        <S.Title>{movie?.title || movie?.name}</S.Title>
-                                        <S.Overview>{movie?.overview.slice(0, 150) || movie?.overview}...</S.Overview>
-                                    </S.BannerWrap>
-                                    <S.BannerWrap>
+                                        <S.Title>{item?.title || item?.name}</S.Title>
+                                        <S.Overview>{item?.overview.slice(0, 150)}...</S.Overview>
                                     </S.BannerWrap>
                                 </S.BannerImage>
                             </S.MainImage>
                         ))}
-                </AnimatePresence>
-            </S.AllWrap>
-        </S.Wrap >
+                </S.AllWrap>
+            </S.Wrap > */}
+            {
+                movies
+                    .filter((item, index) => (index >= 10)) // 10개 이상의 데이터만 가져옴
+                    .map((item, index) => (
+                        <Slide key={item.id}>
+                            <Background imgUrl={item.backdrop_path !== null ? `https://image.tmdb.org/t/p/original${item.backdrop_path}` : "#1d1d1d"} />
+                            <Contents>
+                                <Subtitle>Today's special movie {index + 1}</Subtitle>
+                                <MovieTitle>{item.title}</MovieTitle>
+                                <ViewDetailBtn to={`/movie/${item.id}`}>View detail →</ViewDetailBtn>
+                            </Contents>
+                        </Slide>
+                    ))
+            }
+        </S.StyledSlider >
     );
 }
 
