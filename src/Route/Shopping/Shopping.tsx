83f2c19e59530426e3d3741d0478d851c06afa35
Justin <kidjustinchoi@gmail.com>
2023-01-29 14:59:01 +0900

    add video

--- src/Route/Shopping/Shopping.tsx
+++ src/Route/Shopping/Shopping.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import * as S from "../Style";
 import ReactHlsPlayer from 'react-hls-player';
 import ReactPlayer from "react-player";
 import axios from "axios";
 import { useQuery } from "react-query";
+// import {form} from "../../../videos/movie/jeungei/jeungei.m3u8"
 
 interface GetMovieProps {
     url: string
@@ -15,23 +16,25 @@ interface GetMoviesProps {
 }
 
 function Shopping() {
-    const [movies, setMovies] = useState<GetMovieProps[]>([])
-    const getMovies = async () => {
-        const json = await (await fetch(`http://localhost:3003/movies/all`
-        )).json();
-        console.log(json.movies)
-        setMovies(json.movies)
-    }
-    useEffect(() => {
-        getMovies()
-    }, [])
+    // const [movies, setMovies] = useState<GetMovieProps[]>([])
+    // const getMovies = async () => {
+    //     const json = await (await fetch(`http://localhost:3003/movies/all`
+    //     )).json();
+    //     // console.log(json.movies)
+    //     setMovies(json.movies)
+    // }
+    // useEffect(() => {
+    //     getMovies()
+    // }, [])
+    // ./src/Route/Shopping/videos
     return (
         <S.SearchSliderWrap>
             <S.Title>See you soon! </S.Title>
             {movies.map((movie) => (
                 <ReactPlayer
                     // url={`${movie?.url}`}
-                    url="http://localhost:8000/videos/movie/jeungei/jeungei.m3u8"
+                    url="./videos/jeungei/jeungei.m3u8" 
+                    // to fix this 431 error is to add a cors proxy to the url or to use a different url 
                     playing={false}
                     controls
                     width="60rem"
