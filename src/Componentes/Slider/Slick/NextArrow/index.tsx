e77b6ca454cbb0d93ca621532bbd109db36d8cb5
최근원 <choi138@choegeun-won-ui-iMac.local>
2023-02-07 13:57:50 +0900

    build

--- src/Componentes/Slider/Slick/NextArrow/index.tsx
+++ src/Componentes/Slider/Slick/NextArrow/index.tsx
@@ -2,7 +2,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons"
 
 export const NextArrow = (props: any, slidesToShow: any) => {
-    console.log(slidesToShow, "slidesToShow")
     const { className, onClick, slideCount, currentSlide, style } = props;
     return (
         <>
