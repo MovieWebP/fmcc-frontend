ba93ac687686fb887b0c9fb54c5c68e9dbffd695
choi138 <kidjustinchoi@gmail.com>
2023-02-09 14:18:29 +0900

    bye bye

--- src/Componentes/Detail/modalTop/leftContainter/style.ts
+++ src/Componentes/Detail/modalTop/leftContainter/style.ts
@@ -2,15 +2,10 @@ import styled from "styled-components";
 
 export const LeftContianer = styled.div`   
     display: flex;
-    width: 40rem;
     padding: 20px 0;
     justify-content: space-between;
-    @media screen and (max-width: 1410px) and (min-width: 1100px) {
-        width: 32rem;
-    };
-    @media screen and (max-width: 500px) and (min-width: 300px) {
-        width: 100%;
-    };
+    flex-wrap: wrap;
+    /* border: 1px solid red; */
 `;
 
 export const ModalImage = styled.img`
